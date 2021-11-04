313f3da39cd6891879140a4d68862d84aba4f75c
edrianjansolatorio <edrian.solatorio@arbcalls.com>
2021-11-05 05:35:54 +0800

    push

--- resources/views/pages/private/user-management/Users.tsx
+++ resources/views/pages/private/user-management/Users.tsx
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from 'react'
 import { Typography } from '@mui/material'
 import ContentHeader from '../../../component/core/ContentHeader'
 import DataTable from '../../../component/core/DataTable'
+import MUIDataTable from "mui-datatables";
+import Http from '../../../../api/Api'
 
 const trow = [
  {
@@ -26,9 +28,12 @@ const trow = [
  }
 ];
 
+const columns = ["name", "email", "email_verified_at", "created_at", "updated_at"];
+
 const Users = () => {
  const [thColumn, setThColumn] = useState(trow)
  const [mounted, setMounted] = useState(false)
+ const [data, setData] = useState([])
 
  useEffect(() => {
   setMounted(true)
@@ -37,20 +42,23 @@ const Users = () => {
   }
  }, [])
 
- const Table = ({ }) => {
-  if (mounted) {
-   return (
-    <DataTable url="user" trow={thColumn} />
-   )
-  } else {
-   return <></>
+ const options: any = {
+  serverSide: true,
+  onTableChange: async (action: any, tableState: any) => {
+   const { data } = await Http.get('/auth/data/user/0/5/created_at/asc');
+   setData(data.result)
   }
- }
+ };
 
  return (
   <>
    <ContentHeader title="User list" desc="Where you can see the all users (Restricted for dev only or user)" />
-   <Table />
+   <MUIDataTable
+    title={"Employee List"}
+    data={data}
+    columns={columns}
+    options={options}
+   />
   </>
  )
 }
